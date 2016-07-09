ffcfec8f001551c11a945d4ab92c53e2259412bf
wfnuser <wfnuser@126.com>
2016-07-10 07:28:49 +0800

    add timer addon to quiz page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 require('../css/base.less');
 require('../css/test.less')
 
-var scienceclass = angular.module('scienceclass', ['ui.bootstrap', 'ui.router', 'ngMaterial','ngSanitize']);
+var scienceclass = angular.module('scienceclass', ['ui.bootstrap', 'ui.router', 'ngMaterial','ngSanitize', 'timer']);
 scienceclass.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider, $httpProvider) {
   $httpProvider.defaults.withCredentials = true;
   $mdThemingProvider.setDefaultTheme('altTheme');
