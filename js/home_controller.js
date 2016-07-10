511b42df312d2cc132d4667a1041ad0f6fd1fe12
wfnuser <wfnuser@126.com>
2016-07-10 08:14:52 +0800

    add home_navigator

--- js/home_controller.js
+++ js/home_controller.js
@@ -1,16 +1,18 @@
-var HomeCtrl = function ($scope, $http,$state, User) {
+var HomeCtrl = function ($scope, $http,$state, User, $rootScope) {
+  $rootScope.headershow = true;
 
-  $scope.ttt=-1;
   $scope.submit = function () {
         //todo
         window.location.href = '#/study/studyppt';
         //$state.go('study');
   };
+
   $scope.check = function () {
         //todo
         window.location.href = '#/home';
         //$state.go('study');
   };
+
 }
 
 module.exports = HomeCtrl;
